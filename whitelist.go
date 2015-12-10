b49348bc5db1b1af41e071e44e7987888b9fcb4e
Matt Freeman <matt@nowprovision.com>
2015-12-10 15:44:44 +0700

    enable filtering support

--- whitelist.go
+++ whitelist.go
@@ -5,23 +5,25 @@ import "fmt"
 import "net"
 import "net/http"
 
-func Protect(ipNetworks []*net.IPNet, handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+func Protect(enabled bool, ipNetworks []*net.IPNet, handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		valid := false
-		clientIp := net.ParseIP(strings.Split(r.RemoteAddr, ":")[0])
+		if enabled {
+			valid := false
+			clientIp := net.ParseIP(strings.Split(r.RemoteAddr, ":")[0])
 
-		for _, ipNetwork := range ipNetworks {
-			if ipNetwork.Contains(clientIp) {
-				valid = true
-				break
+			for _, ipNetwork := range ipNetworks {
+				if ipNetwork.Contains(clientIp) {
+					valid = true
+					break
+				}
 			}
-		}
 
-		if !valid {
-			w.WriteHeader(403)
-			fmt.Fprintf(w, "%s", "IP not in whitelist")
-			return
+			if !valid {
+				w.WriteHeader(403)
+				fmt.Fprintf(w, "%s", "IP not in whitelist")
+				return
+			}
 		}
 
 		handler(w, r)
